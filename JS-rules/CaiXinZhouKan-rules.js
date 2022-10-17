c3f7f9eb8530b9cbdf51d22251f5626f88789d5d
githubwisdom <115621427+githubwisdom@users.noreply.github.com>
2022-10-17 17:55:07 +0800

    Create CaiXinZhouKan-rules.js

---
+++ JS-rules/CaiXinZhouKan-rules.js
@@ -0,0 +1,2 @@
+# 财新周刊 解锁会员文章（by公众号墨鱼手记）
+http://ipadcms.caixin.com/tmp/articles/* url script-response-body https://raw.githubusercontent.com/githubwisdom/My-Quantumult-X/main/JS-file/CaiXinZhouKan.js
