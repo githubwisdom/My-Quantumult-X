e4e1a5e8662773bc7da926a8399d315630f34402
githubwisdom <115621427+githubwisdom@users.noreply.github.com>
2022-10-17 18:15:52 +0800

    Update FT-Chinese-rules.js

--- JS-rules/FT-Chinese-rules.js
+++ JS-rules/FT-Chinese-rules.js
@@ -4,3 +4,5 @@
 FT中文网=type=http-response,pattern=^https?:\/\/user\.ftmailbox\.cn\/ios-receipt-validation\.php.*,requires-body=1,script-path=https://raw.githubusercontent.com/githubwisdom/My-Quantumult-X/main/JS-file/TF-Chinese.js
 [MITM]
 hostname= %APPEND% user.ftmailbox.cn
+
+#注：需在分流中新建规则，将danla2f5eudt1.cloudfront.net设置为直连，不然打不开页面
