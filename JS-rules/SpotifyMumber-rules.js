7ae767564ff31eea39b14a70c3bacdc80aad6d6e
githubwisdom <115621427+githubwisdom@users.noreply.github.com>
2022-10-17 18:27:31 +0800

    Update SpotifyMumber-rules.js

--- JS-rules/SpotifyMumber-rules.js
+++ JS-rules/SpotifyMumber-rules.js
@@ -1,5 +1,6 @@
+#解锁Spotify初级会员
 [rewrite_local]
-^https:\/\/spclient\.wg\.spotify\.com\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$ url script-response-body https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-proto.js
+^https:\/\/spclient\.wg\.spotify\.com\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$ url script-response-body https://raw.githubusercontent.com/githubwisdom/My-Quantumult-X/main/JS-file/SpotifyMumber.js
 
 [mitm]
 hostname = spclient.wg.spotify.com
