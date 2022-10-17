ed29bfe60b1d087eda04508c9f9a4443929968f7
githubwisdom <115621427+githubwisdom@users.noreply.github.com>
2022-10-17 18:46:19 +0800

    Update MeiTuXiuXiu-rules.js

--- JS-rules/MeiTuXiuXiu-rules.js
+++ JS-rules/MeiTuXiuXiu-rules.js
@@ -1,3 +1,7 @@
 # 美图秀秀(2022.01.17)    api.xiuxiu.meitu.com, h5.xiuxiu.meitu.com
 # 注：解锁高级会员（包括但不限于：付费海报模板、高级滤镜、高级素材等），无须登陆
+[rewrite_local]
 ^https?:\/\/(h5|api)\.xiuxiu\.meitu\.com\/v\d\/(h\d\/vip|vip|user)\/ url script-response-body https://raw.githubusercontent.com/githubwisdom/My-Quantumult-X/main/JS-file/MeiTuXiuXiu.js
+
+[mitm]
+api.xiuxiu.meitu.com, h5.xiuxiu.meitu.com
