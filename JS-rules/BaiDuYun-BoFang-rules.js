b15991eb26ddc2c6abe61e4f4922321a673f7992
githubwisdom <115621427+githubwisdom@users.noreply.github.com>
2022-10-28 10:16:31 +0800

    Update BaiDuYun-BoFang-rules.js

--- JS-rules/BaiDuYun-BoFang-rules.js
+++ JS-rules/BaiDuYun-BoFang-rules.js
@@ -1,7 +1,6 @@
 #百度网盘 解锁在线视频倍率/清晰度
 
 [rewrite_local]
-https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
-
+https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user url script-response-body https://raw.githubusercontent.com/githubwisdom/My-Quantumult-X/main/JS-file/BaiDuYun-BoFang.js
 [mitm]
 hostname = pan.baidu.com
