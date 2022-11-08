32e308b0c07df8115b0919fd8fdd6a7651ad258f
githubwisdom <115621427+githubwisdom@users.noreply.github.com>
2022-11-08 16:51:20 +0800

    Update HuangYouXiangJi-rules.js

--- JS-rules/HuangYouXiangJi-rules.js
+++ JS-rules/HuangYouXiangJi-rules.js
@@ -1,20 +1,12 @@
-/******************************
 
-脚本功能：黄油相机+解锁VIP
-下载地址：http://mtw.so/5tlBLV
-软件版本：8.18.0
-脚本作者：彭于晏
-更新时间：2022-9-5
-问题反馈：QQ+89996462
-TG群：https://t.me/plus8889
-使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-*******************************/
+#!name=黄油相机
+#!desc=黄油相机解锁会员
+#!homepage=https://github.com/deezertidal
+#!author= 89996462
+#!icon=https://raw.githubusercontent.com/deezertidal/private/main/icons/hyxj.png
 
-[rewrite_local]
+
+hostname = api4.bybutter.com
+
 
 https:\/\/api4\.bybutter\.com\/v4\/* url script-response-body https://raw.githubusercontent.com/githubwisdom/My-Quantumult-X/main/JS-file/HuangYouXiangJi.js
-
-
-
-[MITM]
-hostname = %APPEND% api4.bybutter.com
